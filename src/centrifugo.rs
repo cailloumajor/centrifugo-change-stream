36c3264dd628cce11f67427f2dc0f0d19004d48f
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-11-10 15:08:17 +0000

    chore: add tests for centrifugo module

--- src/centrifugo.rs
+++ src/centrifugo.rs
@@ -3,9 +3,12 @@ use clap::Args;
 use futures_util::FutureExt;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Map, Value};
-use tracing::{debug, debug_span, error, instrument, Instrument};
+use tracing::{debug, info_span, instrument, Instrument};
+use trillium_client::Conn;
 use trillium_tokio::TcpConnector;
 use url::Url;
+
+use crate::errors::{TracedError, TracedErrorContext};
 
 type HttpClient = trillium_client::Client<TcpConnector>;
 
@@ -23,31 +26,42 @@ pub(crate) struct Config {
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct ErrorResponse {
-    code: u16,
-    message: String,
+enum PublishResponse {
+    Result {},
+    Error { code: u16, message: String },
 }
 
 #[derive(Clone)]
 pub(crate) struct Client {
     api_url: Url,
-    api_key: String,
+    auth_header: String,
     http: HttpClient,
 }
 
 impl Client {
     pub(crate) fn new(config: &Config) -> Self {
         let http = HttpClient::new().with_default_pool();
+        let auth_header = String::from("apikey ") + config.centrifugo_api_key.as_str();
 
         Self {
             api_url: config.centrifugo_api_url.to_owned(),
-            api_key: config.centrifugo_api_key.to_owned(),
+            auth_header,
             http,
         }
     }
 
+    fn conn(&self) -> Conn<TcpConnector> {
+        self.http
+            .build_conn("POST", self.api_url.to_owned())
+            .with_header("Authorization", self.auth_header.to_owned())
+    }
+
     #[instrument(skip_all)]
-    pub(crate) async fn publish(&self, channel: &str, data: impl Serialize) {
+    pub(crate) async fn publish(
+        &self,
+        channel: &str,
+        data: impl Serialize,
+    ) -> Result<(), TracedError> {
         let json = json!({
             "method": "publish",
             "params": {
@@ -57,49 +71,31 @@ impl Client {
         });
         debug!(%json);
 
-        let auth_header = String::from("apikey ") + self.api_key.as_str();
-        let mut conn = match self
-            .http
-            .post(self.api_url.as_str())
-            .with_header("Authorization", auth_header)
+        let mut conn = self
+            .conn()
             .with_json_body(&json)
-        {
-            Ok(conn) => conn,
-            Err(err) => {
-                error!(during = "serializing command", %err);
-                return;
-            }
-        };
+            .expect("serialization error");
 
-        if let Err(err) = conn.send().await {
-            error!(during="request send", %err);
-            return;
-        };
+        conn.send().await.context_during("request send")?;
 
         let status_code = conn.status().expect("missing status code");
         if !status_code.is_success() {
-            error!(%status_code);
-            return;
+            return Err(TracedError::from_kind(
+                "bad status code",
+                &status_code.to_string(),
+            ));
         }
 
-        let response: Value = match conn.response_json().await {
-            Ok(resp) => resp,
-            Err(err) => {
-                error!(during="deserializing response", %err);
-                return;
-            }
-        };
+        let response: PublishResponse = conn
+            .response_json()
+            .await
+            .context_during("deserializing response")?;
 
-        if response.get("result").is_none() {
-            if let Some(Ok(error)) = response
-                .get("error")
-                .map(|err_obj| serde_json::from_value::<ErrorResponse>(err_obj.to_owned()))
-            {
-                error!(error.code, error.message, "error from Centrifugo");
-            } else {
-                error!(%response, "unknown response from Centrifugo");
-            };
+        if let PublishResponse::Error { code, message } = response {
+            return Err(TracedError::from_centrifugo_error(code, &message));
         }
+
+        Ok(())
     }
 }
 
@@ -127,9 +123,159 @@ impl Handler<TagsUpdate> for CentrifugoActor {
         async move {
             debug!(msg.namespace, msg.channel_name, ?msg.data);
             let channel = msg.namespace + ":" + msg.channel_name.as_str();
-            client.publish(&channel, msg.data).await;
+
+            if let Err(err) = client.publish(&channel, msg.data).await {
+                err.trace_error();
+            }
         }
-        .instrument(debug_span!("tags update handler"))
+        .instrument(info_span!("tags update handler"))
         .boxed()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod client {
+        use trillium_testing::Method;
+
+        use super::*;
+
+        #[test]
+        fn conn() {
+            let config = Config {
+                centrifugo_api_url: "http://example.com/test".parse().unwrap(),
+                centrifugo_api_key: String::from("secretapikey"),
+            };
+            let client = Client::new(&config);
+            let mut conn = client.conn();
+
+            assert_eq!(conn.method(), Method::Post);
+            assert_eq!(conn.url().as_str(), "http://example.com/test");
+            let auth_header_value = conn
+                .request_headers()
+                .get_values("Authorization")
+                .unwrap()
+                .one()
+                .unwrap();
+            assert_eq!(auth_header_value, "apikey secretapikey");
+        }
+
+        mod publish {
+            use trillium_testing::prelude::Conn;
+
+            use super::*;
+
+            #[test]
+            fn request_send_failure() {
+                async fn handler(_conn: Conn) -> Conn {
+                    panic!()
+                }
+
+                trillium_testing::with_server(handler, |url| async move {
+                    let config = Config {
+                        centrifugo_api_url: url,
+                        centrifugo_api_key: Default::default(),
+                    };
+                    let client = Client::new(&config);
+                    client
+                        .publish("achannel", "somedata")
+                        .await
+                        .expect_err("unexpected publish success");
+
+                    Ok(())
+                })
+            }
+
+            #[test]
+            fn bad_status_code() {
+                async fn handler(conn: Conn) -> Conn {
+                    conn.with_status(500)
+                }
+
+                trillium_testing::with_server(handler, |url| async move {
+                    let config = Config {
+                        centrifugo_api_url: url,
+                        centrifugo_api_key: Default::default(),
+                    };
+                    let client = Client::new(&config);
+                    client
+                        .publish("achannel", "somedata")
+                        .await
+                        .expect_err("unexpected publish success");
+
+                    Ok(())
+                })
+            }
+
+            #[test]
+            fn unknown_response() {
+                async fn handler(conn: Conn) -> Conn {
+                    conn.ok(r#"{"unknown":null}"#)
+                }
+
+                trillium_testing::with_server(handler, |url| async move {
+                    let config = Config {
+                        centrifugo_api_url: url,
+                        centrifugo_api_key: Default::default(),
+                    };
+                    let client = Client::new(&config);
+                    client
+                        .publish("achannel", "somedata")
+                        .await
+                        .expect_err("unexpected publish success");
+
+                    Ok(())
+                })
+            }
+
+            #[test]
+            fn centrifugo_error() {
+                async fn handler(conn: Conn) -> Conn {
+                    conn.ok(r#"{"error":{"code":42,"message":"a message"}}"#)
+                }
+
+                trillium_testing::with_server(handler, |url| async move {
+                    let config = Config {
+                        centrifugo_api_url: url,
+                        centrifugo_api_key: Default::default(),
+                    };
+                    let client = Client::new(&config);
+                    client
+                        .publish("achannel", "somedata")
+                        .await
+                        .expect_err("unexpected publish success");
+
+                    Ok(())
+                })
+            }
+
+            #[test]
+            fn success() {
+                async fn handler(mut conn: Conn) -> Conn {
+                    let req_body = conn.request_body_string().await.unwrap();
+                    assert_eq!(
+                        req_body,
+                        r#"{"method":"publish","params":{"channel":"achannel","data":"somedata"}}"#
+                    );
+                    conn.ok(r#"{"result":{}}"#)
+                }
+
+                trillium_testing::with_server(handler, |url| async move {
+                    let config = Config {
+                        centrifugo_api_url: url,
+                        centrifugo_api_key: Default::default(),
+                    };
+                    let client = Client::new(&config);
+                    client
+                        .publish("achannel", "somedata")
+                        .await
+                        .expect("unexpected publish error");
+
+                    Ok(())
+                })
+            }
+        }
     }
 }
